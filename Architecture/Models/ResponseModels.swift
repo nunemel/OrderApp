e232a1fe9bed306d1cf29bb2eb601e8912ee2205
nunemel <nune.melikyan@yahoo.com>
2022-10-21 18:55:28 +0400

    Add part3 features.

---
+++ Architecture/Models/ResponseModels.swift
@@ -0,0 +1,34 @@
+//
+//  ResponseModels.swift
+//  OrderApp
+//
+//  Created by Nune Melikyan on 21.10.22.
+//
+
+import Foundation
+
+struct MenuResponse: Codable {
+    let items: [MenuItem]
+}
+
+struct CategoriesResponse: Codable {
+    let categories: [String]
+}
+
+struct OrderResponse: Codable {
+    let prepTime: Int
+
+    enum CodingKeys: String, CodingKey {
+        case prepTime = "preparation_time"
+    }
+}
+
+struct Order: Codable {
+    var menuItems: [MenuItem]
+
+    init(
+        menuItems: [MenuItem] = []
+    ) {
+        self.menuItems = menuItems
+    }
+}
